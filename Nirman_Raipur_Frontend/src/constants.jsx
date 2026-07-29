a868c2ad967501dd3ec01de8038e9795ca681e73
root <root@srv1295608.hstgr.cloud>
2026-07-29 08:34:14 +0000

    feat: ch

--- Nirman_Raipur_Frontend/src/constants.jsx
+++ Nirman_Raipur_Frontend/src/constants.jsx
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 export const BASE_SERVER_URL = 
   import.meta.env.VITE_API_URL || 
   (typeof window !== "undefined" && window.location.hostname !== "localhost" && window.location.hostname !== "127.0.0.1" 
     ? "https://nirmanapi.rdmp.in/api" 
     : "http://localhost:3000/api");
+=======
+// export const BASE_SERVER_URL = "http://localhost:3000/api"
+export const BASE_SERVER_URL = "https://nirmanapi.rdmp.in/api"
+>>>>>>> f8c3d02 (feat: ch)
